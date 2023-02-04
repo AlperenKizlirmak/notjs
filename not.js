7037cc9cfdcc1e0d617dffc4d4c96ee29e83a50e
AlperenKizlirmak <118993006+AlperenKizlirmak@users.noreply.github.com>
2023-02-05 00:09:21 +0300

    Add files via upload

---
+++ not.js
@@ -0,0 +1,69 @@
+// const puan = Number(prompt("Lütfen Notunuzu Giriniz : "))
+// if(puan <= 25) {
+//     console.log("FF")
+// }else if(puan <= 42) {
+//     console.log("DD")
+// }else if(puan <= 65) {
+//     console.log("CC")
+// }else if(puan <= 75) {
+//     console.log("BB")
+// }else if(puan <= 90) {
+//     console.log("BA")
+// }else if(puan <= 100) {
+//     console.log("AA")
+// }else {
+//     console.log("Yanlış Değer Girdiniz")
+// }
+// const myFunc = function (value1, value2) {
+//     return value1 + value2
+// }
+// console.log(myFunc(5, 8))
+// const myFunc = (value1, value2) => {
+//     return value1 - value2
+// }
+// const calcArea = (taban, yükseklik) => {
+//     return (taban * yükseklik) / 2
+// }
+// console.log(calcArea(6, 8))
+// console.log(calcArea(10, 20))
+// const calcAge = (yıl) => {
+//     return yıl * 365
+// }
+// console.log(calcAge(65))
+// const calcMonth = () => {
+//     ay = prompt("Lütfen Ay İsmini Giriniz :")
+//     switch (ay) {
+//         case "Ocak":
+//             return 1
+//             break;
+//         case "Şubat":
+//             return 2
+//             break;
+//         case "Mart":
+//             return 3
+//             break;
+//     }
+// }
+// let letters = "qazxswedcvfrtgbnhyujmökıolçşipğü"
+// let number1 = prompt("Lütfen istediğiniz sayıyı giriniz : ")
+// if(isNaN(number1)) {
+//     console.log("Girdiğiniz Karakter Sayı Değil")
+// }else {
+//     if(number1 % 2) {
+//         console.log("Girilen Sayı Tektir")
+//     } else {
+//         console.log("Girilen Sayı Çifttir")
+//     }
+// }
+// const convertToFahreneit = (degree) => {
+//     return (degree * 1.8) + 32
+// }
+// console.log(convertToFahreneit(32))
+// let degree = prompt("Lütfen Dereceyi Giriniz");
+// console.log((degree * 1.8) + 32)
+// console.log(convertToFahreneit(56))
+// console.log(convertToFahreneit(-10))
+// let convertToFahreneit = function() {
+// }
+// const convertToFahreneit = (p1, p2, p3) => {
+// }
